cddb65e58dcbb1c26f48b11c81f4e0669aea8b37
zakerr022 <zakerr022@student.wethinkcode.co.za>
2023-05-20 21:56:12 +0200

    added background music

---
+++ src/public/assets/Denes_2d_side_scroller_character_concept_2D_girl_game_character_05dc392b-4ea7-4d75-bd62-ad4887b29dbd.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="Denes_2d_side_scroller_character_concept_2D_girl_game_character_05dc392b-4ea7-4d75-bd62-ad4887b29dbd" tilewidth="32" tileheight="32" tilecount="1024" columns="32">
+ <image source="../../../../../Downloads/2d character fantasy/Denes_2d_side_scroller_character_concept_2D_girl_game_character_05dc392b-4ea7-4d75-bd62-ad4887b29dbd.png" width="1024" height="1024"/>
+</tileset>
---
+++ src/public/assets/Denes_2d_side_scroller_character_concept_2D_girl_game_character_3bd3641a-330e-46a1-a7e2-8e44e2832aff.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="Denes_2d_side_scroller_character_concept_2D_girl_game_character_3bd3641a-330e-46a1-a7e2-8e44e2832aff" tilewidth="32" tileheight="32" tilecount="1024" columns="32">
+ <image source="../../../../../Downloads/2d character fantasy/Denes_2d_side_scroller_character_concept_2D_girl_game_character_3bd3641a-330e-46a1-a7e2-8e44e2832aff.png" width="1024" height="1024"/>
+</tileset>
